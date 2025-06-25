f51bd1871ca97b3b4a2a4fc4c8c750b1df7814f0
Vladyslav Vaskov <vaskov.vlad@gmail.com>
2025-06-25 15:55:33 +0100

    feat: add useLocation hook

--- nvsoftlab-app/src/App.jsx
+++ nvsoftlab-app/src/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import DiceRoll from "./pages/DiceRoll";
 import { createPageUrl } from "./utils";
 
+function LayoutWithLocation({ children }) {
+  const location = useLocation();
+  let currentPageName = "Home";
+
+  if (location.pathname === createPageUrl("DiceRoll")) {
+    currentPageName = "DiceRoll";
+  } else if (location.pathname === createPageUrl("Home")) {
+    currentPageName = "Home";
+  }
+  return <Layout currentPageName={currentPageName}>{children}</Layout>;
+}
+
 export default function App() {
   return (
     <Router>
-      <Layout>
+      <LayoutWithLocation>
         <Routes>
           <Route path={createPageUrl("Home")} element={<Home />} />
           <Route path={createPageUrl("DiceRoll")} element={<DiceRoll />} />
           <Route path="*" element={<Home />} />
         </Routes>
-      </Layout>
+      </LayoutWithLocation>
     </Router>
   );
 }
